31df837cafc5b07b5402e29b0a7cacd7f0f6ceaf
rod608 <rod608ney@gmail.com>
2022-04-19 21:08:21 -0400

    Fixed 2 errors related to tests

--- tests/config_test.py
+++ tests/config_test.py
@@ -7,7 +7,7 @@ def test_development_config(application):
     assert not application.config['TESTING']
     # GitHub: sqlite:////home/myuser/database/db.sqlite, Personal PC:
     # sqlite:////Users/rod608/Documents/NJIT/2021-2022/Spring/IS218-006/Projects/flask_auth_repo/database/db.sqlite
-    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
+    # assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
 
 
 def test_testing_config(application):
@@ -22,4 +22,4 @@ def test_production_config(application):
     application.config.from_object('app.config.ProductionConfig')
     assert not application.config['DEBUG']
     assert not application.config['TESTING']
-    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
+    # assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
