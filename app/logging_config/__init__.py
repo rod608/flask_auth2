3f26c01f0697f26f68c25d58dbeaa737f05874ba
rod608 <rod608ney@gmail.com>
2022-04-29 08:15:31 -0400

    Added some files

---
+++ app/logging_config/__init__.py
@@ -0,0 +1,154 @@
+import logging
+import os
+from logging.config import dictConfig
+
+import flask
+from flask import request, current_app
+
+# from app.logging_config.log_formatters import RequestFormatter
+from app import config
+
+log_con = flask.Blueprint('log_con', __name__)
+
+
+@log_con.before_app_first_request
+def setup_logs():
+    # set the name of the apps log folder to logs
+    logdir = config.Config.LOG_DIR
+    # make a directory if it doesn't exist
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    logging.config.dictConfig(LOGGING_CONFIG)
+    # Log Setup 4 myerrors
+    current_app.logger.info("Debug-level log function reached: myerrors logger activated.")
+    log = logging.getLogger("myerrors")
+    log.info("Debug-level log function reached: myerrors logger activated. ")
+
+
+@log_con.before_app_request
+def before_request_logging():
+    current_app.logger.info("Before Request")
+
+    log = logging.getLogger("request")
+    log.info("Before Request")
+
+    log2 = logging.getLogger("myerrors")
+    log2.info("Debug-level log function reached: myerrors logger activated. ")
+
+
+@log_con.after_app_request
+def after_request_logging(response):
+    # Request Logger Setup
+    current_app.logger.info("After Request")
+    log = logging.getLogger("request")
+
+    if request.path == '/favicon.ico':
+        log.info("After Favicon Request")
+        return response
+    elif request.path.startswith('/static'):
+        log.info("After Static Request")
+        return response
+    elif request.path.startswith('/bootstrap'):
+        log.info("After Bootstrap Request")
+        return response
+    log.info("After General Request")
+    return response
+
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',  # Default is stderr
+        },
+        'file.handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'handler.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.myapp': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'myapp.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.request': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'request.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.errors': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'errors.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.sqlalchemy': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'sqlalchemy.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.werkzeug': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'werkzeug.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['default','file.handler'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        '__main__': {  # if __name__ == '__main__'
+            'handlers': ['default','file.handler'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'werkzeug': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.werkzeug'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'sqlalchemy.engine': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.sqlalchemy'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'myApp': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.myapp'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'myerrors': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.errors'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'request': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.request'],
+            'level': 'DEBUG',
+            'propagate': False
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Project Setup
+
+[![Production Workflow](https://github.com/rod608/flask_auth2/actions/workflows/prod.yml/badge.svg)](https://github.com/rod608/flask_auth2/actions/workflows/prod.yml)
+
+* [Production Deployment](https://ren9-p2-prod.herokuapp.com/)
+
+
+[![Development Workflow](https://github.com/rod608/flask_auth2/actions/workflows/dev.yml/badge.svg)](https://github.com/rod608/flask_auth2/actions/workflows/dev.yml)
+
+* [Developmental Deployment](https://ren9-p2-dev.herokuapp.com/)
+
+
+## Running Locally
+
+1. To Build with docker compose:
+   docker compose up --build
+2. To run tests, Lint, and Coverage report use this command: pytest --pylint --cov
+
+.pylintrc is the config for pylint, .coveragerc is the config for coverage and setup.py is a config file for pytest
+
+
+### Future Notes and Resources
+* https://flask-user.readthedocs.io/en/latest/basic_app.html
+* https://hackersandslackers.com/flask-application-factory/
+* https://suryasankar.medium.com/a-basic-app-factory-pattern-for-production-ready-websites-using-flask-and-sqlalchemy-dbb891cdf69f
+* https://develie.hashnode.dev/exploring-flask-sqlalchemy-queries
+* https://wtforms.readthedocs.io/en/3.0.x/
+* https://bootstrap-flask.readthedocs.io/en/stable/
+* https://flask-sqlalchemy.palletsprojects.com/en/2.x/
